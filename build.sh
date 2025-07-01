0190d929baa2321fcc1d3fa235cffce6a3d94690
H. Joe Lee <hyoklee@hdfgroup.org>
2025-06-30 23:42:55 -0500

    build(conda): fix script

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
 ./prepare.sh
+export CMAKE_PREFIX_PATH=$PREFIX
+git clone --single-branch https://github.com/hyoklee/vcpkg
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg install argobots
+./vcpkg install mercury
+./vcpkg integrate install
+cd ..
+./configure PKG_CONFIG_PATH=./vcpkg/installed/x64-linux/lib/pkgconfig:~/miniconda3/lib/pkgconfig/ CFLAGS="-g -Wall" --prefix=$PREFIX
+make
+make install
+
