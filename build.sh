0fc4e3eb4108d9c5a0439c99c2d52b518e3b3cea
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-02 22:39:14 -0500

    build: use PIC

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./prepare.sh
 export CMAKE_PREFIX_PATH=$PREFIX
-./configure PKG_CONFIG_PATH=./vcpkg/installed/x64-linux/lib/pkgconfig:$CONDA_PREFIX/lib/pkgconfig/ CFLAGS="-g -Wall" --prefix=$PREFIX
+./configure PKG_CONFIG_PATH=./vcpkg/installed/x64-linux/lib/pkgconfig:$CONDA_PREFIX/lib/pkgconfig/ CFLAGS="-g -Wall -fPIC" --prefix=$PREFIX
 make
 make install
 
