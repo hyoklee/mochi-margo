301c6fcdba3e2bedb13438b7f69db0b3256f90f3
H. Joe Lee <hyoklee@hdfgroup.org>
2025-07-02 22:04:47 -0500

    build(conda): remove vcpkg

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 ./prepare.sh
 export CMAKE_PREFIX_PATH=$PREFIX
-git clone --single-branch https://github.com/hyoklee/vcpkg
-cd vcpkg
-./bootstrap-vcpkg.sh
-./vcpkg install argobots
-./vcpkg install mercury
-./vcpkg integrate install
-cd ..
 ./configure PKG_CONFIG_PATH=./vcpkg/installed/x64-linux/lib/pkgconfig:$CONDA_PREFIX/lib/pkgconfig/ CFLAGS="-g -Wall" --prefix=$PREFIX
 make
 make install
